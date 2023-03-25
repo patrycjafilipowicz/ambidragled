c652eccccb921c83c08fa5a753e0b727395f7062
Pat <patrycja.filipowicz46@gmail.com>
2023-03-25 19:50:52 +0100

    button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-	return <div>
-    <div className="screen">
-    </div>
-  </div>;
+	const [leds, setLeds] = useState(["led"]);
+
+	const mappedAddElement = leds.map((element, index) => {
+		return <div className='led'>{element}</div>;
+	});
+
+	return (
+		<div>
+			<div className='screen'>
+				<div>{mappedAddElement}</div>
+			</div>
+			<button
+				onClick={() => {
+					setLeds(leds => [...leds, "led"]);
+				}}>
+				Add led
+			</button>
+		</div>
+	);
 }
+
 
 export default App;
