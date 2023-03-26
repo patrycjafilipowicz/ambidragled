9595dd3657391f1c0ca83507372ef01425ee6459
Pat <patrycja.filipowicz46@gmail.com>
2023-03-26 20:53:41 +0200

    mouse movement tracking

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
 	const [leds, setLeds] = useState(["led"]);
+	const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+	useEffect(() => {
+		const handleWindowMouseMove = event => {
+			setCoords({
+				x: event.clientX,
+				y: event.clientY,
+			});
+		};
+		window.addEventListener("mousemove", handleWindowMouseMove);
+
+		return () => {
+			window.removeEventListener("mousemove", handleWindowMouseMove);
+		};
+	}, []);
 
 	const mappedAddElement = leds.map((element, index) => {
 		return <div className='led'>{element}</div>;
@@ -13,6 +28,14 @@ function App() {
 			<div className='screen'>
 				<div>{mappedAddElement}</div>
 			</div>
+			<div className='coords'>
+				<p>
+					Mouse positioned at:{" "}
+					<b>
+						({coords.x}, {coords.y})
+					</b>
+				</p>
+			</div>
 			<button
 				onClick={() => {
 					setLeds(leds => [...leds, "led"]);
@@ -22,6 +45,5 @@ function App() {
 		</div>
 	);
 }
-
 
 export default App;
