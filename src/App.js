5c138ad63175910915d8bde09591bd36db831296
Pat <patrycja.filipowicz46@gmail.com>
2023-03-27 20:12:07 +0200

    led is moving within boundaries of the screen

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
 	const [leds, setLeds] = useState(["led"]);
 	const [coords, setCoords] = useState({ x: 0, y: 0 });
+	const [pressedElementIndex, setPressedElementIndex] = useState(null);
+	const screen = useRef();
 
 	useEffect(() => {
+		const screenRef = screen.current;
+		const position = screenRef.getBoundingClientRect();
 		const handleWindowMouseMove = event => {
+			console.log(event);
 			setCoords({
-				x: event.clientX,
-				y: event.clientY,
+				x: event.pageX - position.left,
+				y: event.pageY - position.top,
 			});
 		};
-		window.addEventListener("mousemove", handleWindowMouseMove);
+		screenRef.addEventListener("mousemove", handleWindowMouseMove);
 
 		return () => {
-			window.removeEventListener("mousemove", handleWindowMouseMove);
+			screenRef.removeEventListener("mousemove", handleWindowMouseMove);
 		};
-	}, []);
+	}, [screen]);
 
 	const mappedAddElement = leds.map((element, index) => {
-		return <div className='led'>{element}</div>;
+		return (
+			<div
+				style={{
+					position: "absolute",
+					left: `${coords.x}px`,
+					top: `${coords.y}px`,
+				}}
+				className='led'
+				onClick={() => {
+					setPressedElementIndex(index);
+				}}>
+				{element}
+			</div>
+		);
 	});
 
 	return (
 		<div>
-			<div className='screen'>
+			<div ref={screen} className='screen'>
 				<div>{mappedAddElement}</div>
 			</div>
 			<div className='coords'>
