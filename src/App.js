2f8840a5382aa592f5990cf0339339234f000de7
Pat <patrycja.filipowicz46@gmail.com>
2023-04-04 19:02:32 +0200

    fixes for comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import React, { useState, useEffect, useRef } from "react";
+const ledHeight = 30;
+const ledWidth = 50;
+const border = 10;
+const initialX = 33;
+const initialY = 25;
+const mousemove = "mousemove";
+const zero = 0;
+const halfLedWidth = 25;
+const halfLedHeight = 15;
 
 function App() {
-	const [leds, setLeds] = useState([{ x: 33, y: 25 }]);
+	const [leds, setLeds] = useState([{x: initialX, y: initialY }]);
 	// Coordinates are relative to parent box top left corner
-	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+	const [mousePosition, setMousePosition] = useState({ x: zero, y: zero });
 	const [pickedLedIndex, setPickedLedIndex] = useState(null);
 	const screen = useRef();
 
@@ -14,9 +23,9 @@ function App() {
 		const handleWindowMouseMove = event => {
 			console.log(event);
 			const xAvailableSpace =
-				event.pageX >= (position.left + 10 + 25) && event.pageX <= (position.right - 10 - 25);
+				event.pageX >= (position.left + border + initialY) && event.pageX <= (position.right - border- initialY);
 			const yAvailableSpace =
-				event.pageY >= (position.top + 10 + 15) && event.pageY <= (position.bottom - 10 -15);
+				event.pageY >= (position.top + border + halfLedHeight) && event.pageY <= (position.bottom - border -halfLedHeight);
 			if (xAvailableSpace && yAvailableSpace) {
 				setMousePosition({
 					x: event.pageX - position.left,
@@ -24,10 +33,10 @@ function App() {
 				});
 			}
 		};
-		screenRef.addEventListener("mousemove", handleWindowMouseMove);
+		screenRef.addEventListener(mousemove, handleWindowMouseMove);
 
 		return () => {
-			screenRef.removeEventListener("mousemove", handleWindowMouseMove);
+			screenRef.removeEventListener(mousemove, handleWindowMouseMove);
 		};
 	}, [screen]);
 
@@ -38,13 +47,13 @@ function App() {
 					pickedLedIndex === index
 						? {
 								position: "absolute",
-								left: `${mousePosition.x - 33}px`,
-								top: `${mousePosition.y - 25}px`,
+								left: `${mousePosition.x - initialX}px`,
+								top: `${mousePosition.y - initialY}px`,
 						  }
 						: {
 								position: "absolute",
-								left: `${element.x - 33}px`,
-								top: `${element.y - 25}px`,
+								left: `${element.x - initialX}px`,
+								top: `${element.y - initialY}px`,
 						  }
 				}
 				className='led'
@@ -78,7 +87,7 @@ function App() {
 			</div>
 			<button
 				onClick={() => {
-					setLeds(leds => [...leds, { x: 33, y: 25 }]);
+					setLeds(leds => [...leds, { x: initialX, y: initialY }]);
 				}}>
 				Add led
 			</button>
